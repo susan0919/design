3e78606305dcaea0eefb1fb67ea27047ce77e351
wend <wendan@autoai.com>
2021-03-26 16:03:11 +0800

    commit10

--- app/src/androidTest/java/com/example/design/launcher/fragment/AppMenuFragmentTest.java
+++ app/src/androidTest/java/com/example/design/launcher/fragment/AppMenuFragmentTest.java
@@ -1,5 +1,40 @@
+package com.example.design.launcher.fragment;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AppMenuFragmentTest {
 
+    @Test
+    public void fetchData() {
+    }
+
+    @Test
+    public void layoutId() {
+    }
+
+    @Test
+    public void initView() {
+    }
+
+    @Test
+    public void setListener() {
+    }
+
+    @Test
+    public void injectPresenter() {
+    }
+
+    @Test
+    public void initData() {
+    }
+
+    @Test
+    public void updateAppListView() {
+    }
+
+    @Test
+    public void onDestroy() {
+    }
 }
--- app/src/test/java/com/example/design/ExampleUnitTest.java
+++ app/src/test/java/com/example/design/ExampleUnitTest.java
@@ -1,17 +1,53 @@
 package com.example.design;
 
+
+import com.example.design.launcher.presenter.AppListPresenter;
+import com.example.design.launcher.view.IAppListView;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.List;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest implements IAppListView {
+    private AppListPresenter presenter;
+
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        A a = new A();
+//        presenter = new AppListPresenter(this, InstrumentationRegistry.getInstrumentation().targetContext);
+
+//        a.aa.a = "88";
+        changePackageInfo(a);
+    }
+
+    public void changePackageInfo(A owner) {
+        B a = owner.aa;
+        a.b = "99";
+//        Log.d("ExampleUnitTest", owner.aa.b);
+
+    }
+
+    @Override
+    public void updateAppListView(List<String> data) {
+
+    }
+
+
+    public class A {
+        public A() {
+            aa = new B();
+        }
+
+        public B aa;
+    }
+
+    public class B {
+        public String b;
+
     }
 }
